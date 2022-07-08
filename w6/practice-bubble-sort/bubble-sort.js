7067f0eab94bc5ad5ea2ab0f01841b065334b0fd
Shane Wilkey <76767429+southwestmogrown@users.noreply.github.com>
2022-07-08 16:43:07 -0500

    bubble sort

--- w6/practice-bubble-sort/bubble-sort.js
+++ w6/practice-bubble-sort/bubble-sort.js
@@ -1,17 +1,30 @@
 
 function bubbleSort(arr) {
+  // create a swapped flag to mark whether a swap has occured
+  let swapped = true;
 
+  while (swapped) {
+    // set swapped to false if a swap does not occur during the iteration
+      // the array must be sorted
+    swapped = false;
     // Iterate through the array
-
+    for (let i = 0; i < arr.length; i++) {
       // If the current value is greater than its neighbor to the right
+      if (arr[i] > arr[i+1]) {
         // Swap those values
-
+        [arr[i], arr[i+1]] = [arr[i+1], arr[i]];
+        // since a swap occured, set swapped back to true
+          // this will force another iteration
+        swapped = true;
         // Do not move this console.log
         console.log(arr.join(","));
-
+      }
+    
+    }
     // If you get to the end of the array and no swaps have occurred, return
-
-    // Otherwise, repeat from the beginning
+    if (!swapped) return arr;
+      // Otherwise, repeat from the beginning
+  }
 
 }
 
