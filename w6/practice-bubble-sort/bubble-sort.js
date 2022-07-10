f949e6855536d56553f5f510e0fa0ed6bc632ef6
Shane Wilkey <76767429+southwestmogrown@users.noreply.github.com>
2022-07-09 21:24:48 -0500

    worked on insertion sort, starting selection sort

--- w6/practice-bubble-sort/bubble-sort.js
+++ w6/practice-bubble-sort/bubble-sort.js
@@ -1,5 +1,7 @@
 
 function bubbleSort(arr) {
+
+  const array = [...arr];
   // create a swapped flag to mark whether a swap has occured
   let swapped = true;
 
@@ -8,24 +10,73 @@ function bubbleSort(arr) {
       // the array must be sorted
     swapped = false;
     // Iterate through the array
-    for (let i = 0; i < arr.length; i++) {
+    for (let i = 0; i < array.length; i++) {
       // If the current value is greater than its neighbor to the right
-      if (arr[i] > arr[i+1]) {
+      if (array[i] > array[i+1]) {
         // Swap those values
-        [arr[i], arr[i+1]] = [arr[i+1], arr[i]];
+        [array[i], array[i+1]] = [array[i+1], array[i]];
         // since a swap occured, set swapped back to true
           // this will force another iteration
         swapped = true;
         // Do not move this console.log
-        console.log(arr.join(","));
+        // console.log(array.join(","));
       }
     
     }
     // If you get to the end of the array and no swaps have occurred, return
-    if (!swapped) return arr;
+    if (!swapped) return array;
       // Otherwise, repeat from the beginning
   }
 
 }
+
+// create a swapped flag to mark whether a swap has occured
+  // set swapped to false if a swap does not occur during the iteration
+    // the array must be sorted
+  // Iterate through the array
+    // If the current value is greater than its neighbor to the right
+      // Swap those values
+      // since a swap occured, set swapped back to true
+        // this will force another iteration
+      // Do not move this console.log
+  // If you get to the end of the array and no swaps have occurred, return
+    // Otherwise, repeat from the beginning
+
+function bubbleSort2(arr) {
+
+  const array = [...arr]
+
+  let swapped = false;
+  for (let i = 0; i < array.length - 1; i++) {
+    for (let j = 0; j < array.length - i; j++) {
+      if (array[j] > array[j+1]) {
+        [array[j], array[j+1]] = [array[j+1], array[j]];
+        // console.log(array.join(","));
+        swapped = true;
+      }
+    }
+    if (!swapped) return array;
+  }
+  return array;
+}
+
+
+
+const arr = [];
+const n = 25000
+
+for (let i = 0; i < n; i++) {
+  arr.push(Math.floor(Math.random() * n));
+}
+
+// console.log(arr)
+
+console.time("Bubble Sort");
+bubbleSort(arr);
+console.timeEnd("Bubble Sort");
+
+console.time("Bubble Sort 2");
+bubbleSort2(arr);
+console.timeEnd("Bubble Sort 2");
 
 module.exports = bubbleSort;
--- w6/practice-for-week-06-insertion-sort/insertion-sort.js
+++ w6/practice-for-week-06-insertion-sort/insertion-sort.js
@@ -1,44 +1,66 @@
-// Insertion Sort out-of-place
-// Do not modify the original array
+// // Insertion Sort out-of-place
+// // Do not modify the original array
+// function insertionSort(arr) {
+//   /*
+//   Pseudocode:
+
+//   Copy the original array
+//   Create an array to store the sorted values
+//   While the array is not empty:
+//   - make sure you have a console.log(sorted.join(',')) as your first line in the while loop
+//   - Pop a value from the array
+//   - Create a new spot at the end of the array with null to help with comparisons
+//   - Walk through the sorted array in reverse order
+//   - Check if the value to the left is smaller than the new value
+//   - If so, you've reached the insertion point so exit the loop
+//   - If not shift the value to the right by 1 and continue
+//   - Insert the unsorted value at the break point
+//   Return the sorted array
+//   */
+
+//   // Your code here
+
+//   // const copy = [...arr];
+//   const sorted = [];
+
+//   while (arr.length) {
+//     console.log(sorted.join(','));
+//     const el = arr.pop();
+//     sorted.push(null);
+//     let i = sorted.length - 1;
+
+//     while (i > 0) {
+//       if (sorted[i-1] <= el) break;
+//       else {
+//         sorted[i] = sorted[i-1];
+//         i--;
+//       }
+//     }
+//     sorted[i] = el;
+//   }
+//   return sorted
+// }
+
 function insertionSort(arr) {
-  /*
-  Pseudocode:
-
-  Copy the original array
-  Create an array to store the sorted values
-  While the array is not empty:
-  - make sure you have a console.log(sorted.join(',')) as your first line in the while loop
-  - Pop a value from the array
-  - Create a new spot at the end of the array with null to help with comparisons
-  - Walk through the sorted array in reverse order
-  - Check if the value to the left is smaller than the new value
-  - If so, you've reached the insertion point so exit the loop
-  - If not shift the value to the right by 1 and continue
-  - Insert the unsorted value at the break point
-  Return the sorted array
-  */
-
-  // Your code here
 
   const copy = [...arr];
-  const sorted = [];
+  let sorted = [];
 
+  
   while (copy.length) {
-    console.log(sorted.join(','));
-    const el = copy.pop();
+    console.log(sorted.join(','))
+    const val = copy.pop();  // 5
     sorted.push(null);
-    let i = sorted.length - 1;
 
-    while (i > 0) {
-      if (sorted[i-1] <= el) break;
-      else {
-        sorted[i] = sorted[i-1];
-        i--;
-      }
+    let i;
+      
+    for (i = sorted.length - 1; i > 0; i--) {
+      if (sorted[i - 1] <= val) break
+      else sorted[i] = sorted[i-1];  
     }
-    sorted[i] = el;
+      sorted[i] = val;
   }
-  return sorted
+  return sorted;
 }
 
 // In-place Insertion Sort
@@ -60,27 +82,41 @@ function insertionSortInPlace(arr) {
   Return the mutated array
   */
 
-  let divider = 1;
+  // let divider = 1;
 
-  while (divider < arr.length) {
-    console.log(arr.join(','));
+  // while (divider < arr.length) {
+  //   console.log(arr.join(','));
 
-    let val = arr[divider];
-    let i = divider;
+  //   let val = arr[divider];
+  //   let i = divider;
 
-    while (i > 0) {
-      if (arr[i-1] > val) {
-        arr[i] = arr[i-1];
-        i--;
-      } else break;
-    }
+  //   while (i > 0) {
+  //     if (arr[i-1] > val) {
+  //       arr[i] = arr[i-1];
+  //       i--;
+  //     } else break;
+  //   }
 
-    arr[i] = val;
-    divider++;
-  }
-  return arr;
+  //   arr[i] = val;
+  //   divider++;
+  // }
+  // return arr;
 
   // Your code here
-}
 
+  // const copy = arr.slice();
+
+  for (let i = 1; i < arr.length; i++) {
+    console.log(arr.join(','));
+    
+    for (let j = i; j > 0; j--) {
+      if (arr[j] < arr[j-1]) {
+        [arr[j-1], arr[j]] = [arr[j], arr[j-1]];
+      } 
+    }
+  }
+  return arr
+  
+}
+console.log(insertionSortInPlace([3, 4, 2, 1, 6, 9, 5, 8, 10, 7]))
 module.exports = [insertionSort, insertionSortInPlace];
