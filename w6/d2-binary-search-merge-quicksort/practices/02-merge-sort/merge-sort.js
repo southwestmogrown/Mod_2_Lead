6b69ebc78daf196a4c764b3ea2ab9f68611f39ae
Shane Wilkey <76767429+southwestmogrown@users.noreply.github.com>
2022-07-12 09:38:10 -0500

    moved things around

--- w6/d2-binary-search-merge-quicksort/practices/02-merge-sort/merge-sort.js
+++ w6/d2-binary-search-merge-quicksort/practices/02-merge-sort/merge-sort.js
@@ -1,8 +1,7 @@
 // Merge Sort out-of-place
 // Do not modify the original array
 function mergeSort(arr) {
-                                                        //            M
-  // Check if the input is length 1 or less             // [10, 1, 5, 2, 8, 7]
+  // Check if the input is length 1 or less             
     // If so, it's already sorted: return
   if (arr.length <= 1) return arr;
   // Divide the array in half
@@ -19,33 +18,34 @@ function mergeSort(arr) {
   // Merge the halves together and return
   return merge(leftSort, rightSort);
 }
-
-                                                        //  LH         RH
-                                                        // [10,1,5]  [2,8,7]
+                                                        //            M
+                                                        // [10, 5, 1, 2, 8, 7]
+                                                        //  LH               RH
+                                                        // [10,5,1]        [2,8,7]
                                                         //    
-                                                        //  [10,1,5]
+                                                        //  [10,5,1]
                                                         //  LH     RH
-                                                        // [10]   [1,5]
+                                                        // [10]   [5,1]
 
                                                         // [10]
 
-                                                        //       [1,5]
+                                                        //        [5,1]
                                                         //        LH    RH
-                                                        //       [1]   [5]
+                                                        //       [5]   [1]
                                                         //       [1,5]
 
                                                         // [1,5,10]
 
-                                                                  // [2,8,7]
-                                                                  // LH    RH
-                                                                  // [2]   [8,7]
+                                                                        // [2,8,7]
+                                                                        // LH    RH
+                                                                        // [2]   [8,7]
 
-                                                                  // [2]
-                                                                  //      LH   RH
-                                                                  //      [8]  [7]
-                                                                  //      [7,8]
+                                                                        // [2]
+                                                                        //      LH   RH
+                                                                        //      [8]  [7]
+                                                                        //      [7,8]
 
-                                                                  // [2,7,8]
+                                                                        // [2,7,8]
                                                         // [1,2,5,7,8,10]
 
 // Takes in two sorted arrays and returns them merged into one
