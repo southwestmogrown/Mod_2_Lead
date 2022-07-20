1435e1b5c3258fda92ba4421672de0bd5d764a44
Shane Wilkey <76767429+southwestmogrown@users.noreply.github.com>
2022-07-20 10:25:35 -0500

    inheritance

--- Module-2-Resources/week-4/d2-es6-classes/practices/03-inheritance/classes/student.js
+++ Module-2-Resources/week-4/d2-es6-classes/practices/03-inheritance/classes/student.js
@@ -1,7 +1,21 @@
 const Person = require('./person');
 
 // Your code here
+class Student extends Person {
+  constructor(firstName, lastName, major, GPA) {
+    super(firstName, lastName);
+    this.major = major
+    this.GPA = GPA;
+  }
 
+  static compareGPA(student1, student2) {
+    return student1.GPA > student2.GPA 
+    ? 
+    `${student1.firstName} ${student1.lastName} has the higher GPA.`
+    : 
+    `${student2.firstName} ${student2.lastName} has the higher GPA.`
+  }
+}
 /****************************************************************************/
 /******************* DO NOT EDIT CODE BELOW THIS LINE ***********************/
 
