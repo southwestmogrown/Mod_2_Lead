c2b61420b2f2f7b60bdd6b5df16a1c686121440a
Shane Wilkey <76767429+southwestmogrown@users.noreply.github.com>
2022-07-20 10:02:06 -0500

    Class Syntax Practice

--- Module-2-Resources/week-4/d2-es6-classes/practices/02-basic-class-syntax/classes/person.js
+++ Module-2-Resources/week-4/d2-es6-classes/practices/02-basic-class-syntax/classes/person.js
@@ -1,4 +1,29 @@
 // Your code here
+class Person {
+  constructor(firstName, lastName, age) {
+    this.firstName = firstName;
+    this.lastName = lastName;
+    this.age = age;
+  }
+
+  introduce() {
+    console.log(`Hi, I'm ${this.firstName} ${this.lastName}, and I'm ${this.age} years old.`)
+  }
+
+  static introducePeople(people) {
+    if (!(people instanceof Array) ) {
+      console.log("introducePeople only takes an array as an argument.");
+      return;
+    }
+    for (let person of people) {
+      if (!(person instanceof Person)) {
+        console.log("All items in array must be Person class instances.");
+        return;
+      }
+      person.introduce();
+    };
+  }
+}
 
 /****************************************************************************/
 /******************* DO NOT EDIT CODE BELOW THIS LINE ***********************/
